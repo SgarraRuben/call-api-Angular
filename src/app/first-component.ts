c40b6b14392aed31c2aa2f2146d41ba94ef1a8c3
Ruben Sgarra <ruben.sgarra@libero.it>
2021-02-26 15:24:40 +0100

    call api con angular

---
+++ src/app/first-component.ts
@@ -0,0 +1,12 @@
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'value',
+    template:'<h2>ciaoneeee sono il primo componente</h2>'
+
+})
+
+export class FirstComponent{
+
+}
