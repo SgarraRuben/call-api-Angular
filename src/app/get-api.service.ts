00c1bd2427b73826d522ba2527958345af0e72e3
Ruben Sgarra <ruben.sgarra@libero.it>
2021-03-02 12:42:38 +0100

    call api

--- src/app/get-api.service.ts
+++ src/app/get-api.service.ts
@@ -9,12 +9,6 @@ export class GetApiService {
 
   constructor(private http: HttpClient) { }
 
-  // getApi(){
-  //   let random=Math.floor((Math.random()*61)+1);
-  //   let apiString='https://swapi.dev/api/people/'+random;
-  //   return this.http.get(apiString);
-  // }
-
   findById(id: number): Observable<any> {
     let resp = this.http.get<any>('https://swapi.dev/api/people/' + id, { observe: 'response' });
     return resp;
